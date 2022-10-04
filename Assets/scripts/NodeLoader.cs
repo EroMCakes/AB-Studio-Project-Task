f6e6a4b8dadd6e56e70e233fd5616763c33f44ed
EroMCakes <salah17afifi@gmail.com>
2022-10-04 15:06:07 +0100

    fixed data parsing need conversion to dictionary

--- Assets/scripts/NodeLoader.cs
+++ Assets/scripts/NodeLoader.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class NodeLoader : MonoBehaviour
 {
-    public Whip myWhip;
+    public Root myWhip;
     void Awake() {
         string loadMyWhip = JsonFileReader.LoadJsonAsResource("data/main.json");
-        myWhip = JsonUtility.FromJson<Whip>(loadMyWhip);
+        myWhip = JsonUtility.FromJson<Root>(loadMyWhip);
+        Debug.Log(myWhip.ToString());
     }
+
+    public IDictionary<string, dynamic> jsonMap = new Dictionary<string, dynamic>();
     
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
+
 
     // Update is called once per frame
     void Update()
